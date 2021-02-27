54f8ad8f71e1f5eb361c2f00e7d150285f889de2
Federico Zito <73373411+Federicojzito@users.noreply.github.com>
2021-02-27 20:16:33 -0300

    Update App.js

--- Ejercicio_3/agenda-app/src/App.js
+++ Ejercicio_3/agenda-app/src/App.js
@@ -1,26 +1,3 @@
-import logo from './logo.svg';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
@@ -35,6 +12,7 @@ import {
   FormGroup,
   ModalFooter,
 } from "reactstrap";
+
 
 const data = [
   { id: 1, nombre: "Federico", apellido: "Zito", sexo: "Masculino" , fechaNacimiento : "28-03-1996", },
